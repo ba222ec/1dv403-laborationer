c18d00f1fd506521d3042688c16380d736ddef69
OpenShift guest <ba222ec@student.lnu.se>
2013-11-18 11:41:18 -0500

    Nu är uppgift 4 helt klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,35 +1,49 @@
 "use strict";
 
 var makePerson = function(persArr){
-/*    
-    console.log(persArr);
-
+    
+    // Validering av argumentet. Argementet måste vara en array som innehåller objekt och
+    // objekten måste ha egenskapen name och minst en av egenskaperna born och age. 
     if (!persArr instanceof Array) {
         throw new Error("FEL! Du måste skicka en array som argument!");
     }
     if (!persArr[0]) {
         throw new Error("FEL! Arrayen innehåller inga objekt!");
     }
-    if (!persArr.every(function (person, index, array) {
-        if ('name' in person) {
-            return true;
-        } else {
-            return false;
-        } }) ) {
-        throw new Error("FEL! Alla objekt i arrayen måste ha egenskaperna name!");
+    if (!persArr.every(
+            function (person) { 
+                if ('name' in person && typeof person.name === 'string') {
+                    return true;
+                } else {
+                    return false;}
+            })
+        ) {
+        throw new Error("FEL! Objekten i arrayen måste ha egenskapen name!");
     }
-    if (!persArr.every(function (person, index, array) { if ('age' in person) {return true;} else {return false;} }) ) {
-        throw new Error("FEL! Alla objekt i arrayen måste ha egenskaperna age!");
+    if (!persArr.every(
+            function (person) {
+                if ('born' in person && typeof person.born === 'string') {
+                    return true;
+                } else if ('age' in person && typeof person.age === 'number') {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+        ) {
+        throw new Error("FEL! Objekten i arrayen måste innehålla någon av egenskaperna born eller age!");  
     }
-*/
-    var allAges = persArr.map(function(person) {return person.age}).sort(function(a, b) { return a - b; });
-    var minAge = allAges[0];
-    var maxAge = allAges[allAges.length - 1];
-    var averageAge = Math.round(allAges.reduce(function (a, b) {return a + b}) / persArr.length);
 
-    var allNames = persArr.map(function(person) {return person.name}).sort(function(a, b) { return a.localeCompare(b); });
-    var stringWithNames = allNames.reduce(function (name1, name2) {return name1 + ", " + name2});
+    // Tar fram den högsta och den minsta åldern samt medelåldern.
+    persArr.sort(function(pers1, pers2) { return pers1.age - pers2.age; });
+    var minAge = persArr[0].age;
+    var maxAge = persArr[persArr.length - 1].age;
+    var averageAge = Math.round(persArr.map(function(person) { return person.age }).reduce( function (a, b) { return a + b }) / persArr.length);    
+
+    // Tar ut alla namn, sorterar med och lägger den i en stäng.
+    var allNames = persArr.map(function(person) { return person.name }).sort(function(a, b) { return a.localeCompare(b); });
+    var stringWithNames = allNames.reduce(function (name1, name2) { return name1 + ", " + name2 });
     
-
-    return {averageAge: averageAge, maxAge: maxAge, minAge: minAge, names: stringWithNames};
+    return { averageAge: averageAge, maxAge: maxAge, minAge: minAge, names: stringWithNames };
 };
+
