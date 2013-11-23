d1daf021de651bb6e09b95257ef396a50637c227
Svante <svante.arvedson@gmail.com>
2013-11-23 23:46:02 +0200

    Message färdig, testning kvar

--- 2-labbymezzage/scripts/message.js
+++ 2-labbymezzage/scripts/message.js
@@ -19,33 +19,47 @@ function Message(message, time) {
         },
         "date": {
             get: function() {
-                return time.toLocaleString();
+                return time;
             },
-            set: function(newTime) {
-                newTime = Date.parse(newTime);
-                if (isNaN(newTime) || (Date.getTime() - newTime) <= 0) {
-                    // Empty!
-                }
-                else {
+            set: function (newTime) {
+                if (!newTime instanceof Date) {
+                    newTime = Date.parse(newTime);
+                } 
+                if (isNaN(newTime) || (Date.now() - newTime) <= 0) {
+                    time = Date.now();
+                } else {
                     time = newTime;
                 }
             }
         }
     });
 
-    time = Date.parse(time);
-    if (isNaN(time) || (Date.getTime() - time) <= 0) {
-        time = new Date();
-    }
-
     this.message = message;
     this.date = time;
 }
 
 Message.prototype.toString = function () {
-    return this.text + " (" + this.date + ")";
+    return this.text + " (" + this.date.toLocaleString() + ")";
 }
 
-Message.prototype.toHTMLText = function () {
-    
+Message.prototype.getHTMLText = function () {
+    var HTMLText = "", i, temp;
+
+    // Replacing "\n" with "<br />"
+    for (i = 0; i < this.message.length; i += 1) {
+        temp = this.message.charAt(i);
+        HTMLText += (function (temp) {
+            if (temp === "\n") {
+                return "<br />";
+            } else {
+                return temp;
+            }
+        })();
+    }
+
+    return HTMLText;
+}
+
+Message.prototype.getDateText = function () {
+    return this.date.toLocaleString();
 }
--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,6 +1,9 @@
 (function() {
     console.log("Min anonyma funktion startar och exekverar. Hipp Hipp Hurra!");
 
+    var mess1 = new Message("MyMessage", Date.now());
+
+    console.log(mess1.toString());
     // init
 
 }());
