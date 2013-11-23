9ab2b7ff6fde5f35e7406ad69a33a2e720118cb1
OpenShift guest <ba222ec@student.lnu.se>
2013-11-23 17:56:19 -0500

    Nu är message.js testad och klar

--- 2-labbymezzage/scripts/message.js
+++ 2-labbymezzage/scripts/message.js
@@ -1,19 +1,19 @@
-function Message(message, time) {
+function Message(text, time) {
 
-    if ( typeof message != "string" || message.trim() === 0) {
+    if ( typeof text != "string" || text.trim() === 0) {
         throw new Error("ERROR! The message must contain a text!");
     }
 
     Object.defineProperties(this, {
-        "text": {
+        "message": {
             get: function() {
-                return message;
+                return text;
             },
-            set: function(newMessage) {
-                if(newMessage.trim().length === 0 || typeof newMessage != "string") {
-                }
-                else {
-                    message = newMessage;
+            set: function(newText) {
+                if (typeof newText !== "string" || newText.trim().length === 0) {
+                    // Empty!
+                } else {
+                    text = newText;
                 }
             }
         },
@@ -22,11 +22,11 @@ function Message(message, time) {
                 return time;
             },
             set: function (newTime) {
-                if (!newTime instanceof Date) {
-                    newTime = Date.parse(newTime);
-                } 
-                if (isNaN(newTime) || (Date.now() - newTime) <= 0) {
-                    time = Date.now();
+                // If newTime isn't a Date-object, a invalid Date-object or a
+                // date in the future, nothing will happend.
+                if (typeof newTime !== "object" || !newTime instanceof Date ||
+                        isNaN(newTime) || (Date.now() - newTime.getTime()) <= 0) {
+                    // Empty!
                 } else {
                     time = newTime;
                 }
@@ -34,32 +34,33 @@ function Message(message, time) {
         }
     });
 
-    this.message = message;
+    this.message = text;
     this.date = time;
 }
 
 Message.prototype.toString = function () {
-    return this.text + " (" + this.date.toLocaleString() + ")";
-}
+    return this.message + " (" + this.getDateText() + ")";
+};
 
 Message.prototype.getHTMLText = function () {
-    var HTMLText = "", i, temp;
+    var HTMLText = "", i;
 
+    function analyzeChar(char) {
+        if (char === "\n") {
+            return "<br />";
+        } else {
+            return char;
+        }
+    }
+    
     // Replacing "\n" with "<br />"
     for (i = 0; i < this.message.length; i += 1) {
-        temp = this.message.charAt(i);
-        HTMLText += (function (temp) {
-            if (temp === "\n") {
-                return "<br />";
-            } else {
-                return temp;
-            }
-        })();
+        HTMLText += analyzeChar(this.message.charAt(i));
     }
 
     return HTMLText;
-}
+};
 
 Message.prototype.getDateText = function () {
     return this.date.toLocaleString();
-}
+};
--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,9 +1,5 @@
 (function() {
-    console.log("Min anonyma funktion startar och exekverar. Hipp Hipp Hurra!");
-
-    var mess1 = new Message("MyMessage", Date.now());
-
-    console.log(mess1.toString());
+    
     // init
-
+    
 }());
