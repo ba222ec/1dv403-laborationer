2dc682d5bc341b55c927e572f14295ec36dd8a79
OpenShift guest <ba222ec@student.lnu.se>
2013-11-24 17:00:37 -0500

    Nu är moment 4 labb 2 klart

--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,6 +1,18 @@
+/*
 (function() {
     // The array should store all the messages
     var messages = [];
    
     
 }());
+*/
+
+var Program = {
+    messages: [],
+    
+    init: function() {
+        
+    }
+};
+
+Program.init();
