812b06842d71e0c3540a90a107d0ed4c5c3c935d
Svante <svante.arvedson@gmail.com>
2013-11-25 16:01:37 +0200

    Klar med moment 5
    Nu är knappen kopplad till ett event och det fungerar att skapa
    meddelanden. Jag har dessutom flyttat hela objektet bort från window, så
    det går finemang att ha flera olika fönster uppe samtidigt.

--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,9 +1,39 @@
-var messageBoard = {
-    // The array should store all the messages
-    messages: [],
-    init: function () {
-        alert("Kanppen fungerar!");
-    }
-};
+(function () {
+    var messageBoard = {
+        // The array should store all the messages
+        messages: [],
+        init: function (message) {
+            try {
+                this.messages.push(new Message(message, new Date()));
+                return this.messages;
+            }
+            catch (e) {
+                // Empty!
+            }
+        }
+    };
 
-// document.getElementById("send-button").addEventListener("click", messageBoard.init());
+    var htmlMessageBoard = document.getElementById("message-board"),
+        submit = document.getElementById("submit-button"),
+        input = document.getElementById("message-area"),
+        nbrOfMessages = document.getElementById("nbr");
+
+    submit.addEventListener("click", function (e) {
+        var output, i;
+
+        e.preventDefault();
+        if ((input.value).trim().length === 0) {
+            return;
+        }
+
+        output = messageBoard.init(input.value);
+        htmlMessageBoard.innerHTML = "";
+        for (i = 0; i < output.length; i += 1) {
+            htmlMessageBoard.innerHTML += output[i] + "<br />";
+        }
+
+        nbrOfMessages.innerHTML = "";
+        nbrOfMessages.innerHTML = "Antal meddelanden: " + output.length;
+        input.value = "";
+    });
+}());
