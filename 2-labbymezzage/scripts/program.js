95fe02f7eb6b6f596bc2f1b01fd92518621a40dc
Svante <svante.arvedson@gmail.com>
2013-11-25 14:33:53 +0200

    Merging klar

--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 var messageBoard = {
     // The array should store all the messages
     messages: [],
@@ -10,35 +7,3 @@ var messageBoard = {
 };
 
 // document.getElementById("send-button").addEventListener("click", messageBoard.init());
-=======
-=======
->>>>>>> 2dc682d5bc341b55c927e572f14295ec36dd8a79
-=======
->>>>>>> 2dc682d5bc341b55c927e572f14295ec36dd8a79
-/*
-(function() {
-    // The array should store all the messages
-    var messages = [];
-   
-    
-}());
-*/
-
-var Program = {
-    messages: [],
-    
-    init: function() {
-        
-    }
-};
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-Program.init();
->>>>>>> 2dc682d5bc341b55c927e572f14295ec36dd8a79
-=======
-Program.init();
->>>>>>> 2dc682d5bc341b55c927e572f14295ec36dd8a79
-=======
-Program.init();
->>>>>>> 2dc682d5bc341b55c927e572f14295ec36dd8a79
