da1ab1f68c939a6b2b4a1d6ac3e09e5c2522ee1f
Svante <svante.arvedson@gmail.com>
2013-11-25 14:23:09 +0200

    Några ändringar

--- 2-labbymezzage/scripts/program.js
+++ 2-labbymezzage/scripts/program.js
@@ -1,6 +1,9 @@
-(function() {
+var messageBoard = {
     // The array should store all the messages
-    var messages = [];
-   
-    
-}());
+    messages: [],
+    init: function () {
+        alert("Kanppen fungerar!");
+    }
+};
+
+// document.getElementById("send-button").addEventListener("click", messageBoard.init());
