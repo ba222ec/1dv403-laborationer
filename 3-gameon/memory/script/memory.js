0fe97fe3ad777d600d101af2b98950799200b3d3
Svante <svante.arvedson@gmail.com>
2013-12-05 18:21:40 +0200

    Lagt till loopar som skapar tabellen
    Nu skapas det en tabell med angiven storlek och bilder läggs till på
    rätt ställen enligt den slumpade arrayen.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,17 +1,42 @@
 ﻿"use strict";
 
 function MemoryBoard(memoryID, rows, cols) {
-    // To save the random array from random.js
+
+    // To save the random array from random.js.
     this.memoryCards = [];
 
-    // Only method to be called from outside the object
+    // The only method to be called from outside the object.
     this.init = function () {
+        // Counters
+        var i, p, k = 0;
+        // DOM-nodes
+        var img, anchor, td, tr,
+            table = document.createElement("table"),
+            div = document.querySelector("div#" + memoryID);
+
         // Generates a random array
         this.memoryCards = RandomGenerator.getPictureArray(rows, cols);
 
+        // Creates the table that contains the memory game.
+        for (i = 1; i <= rows; i += 1) {
+            tr = document.createElement("tr");
+            for (p = 1; p <= cols; p += 1) {
+                img = document.createElement("img");
+                img.setAttribute("src", "pics/" + this.memoryCards[k] + ".png");
+                anchor = document.createElement("a");
+                td = document.createElement("td");
+                anchor.appendChild(img);
+                td.appendChild(anchor);
+                tr.appendChild(td);
+                // The picture counter
+                k += 1;
+            }
+            table.appendChild(tr);
+        }
+        div.appendChild(table);
     };
 }
 
 window.onload = function () {
-    new MemoryBoard("memoryBoard1", 2, 4).init();
+    new MemoryBoard("memoryBoard1", 1, 6).init();
 };
