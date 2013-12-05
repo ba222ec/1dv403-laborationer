b4c567461785a409f960cf3a2158a1f24f683974
Svante <svante.arvedson@gmail.com>
2013-12-05 18:44:50 +0200

    Click-event på a-taggarna
    Nu kan man vända på brickorna med ett click-event.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,11 +5,12 @@ function MemoryBoard(memoryID, rows, cols) {
     // To save the random array from random.js.
     this.memoryCards = [];
 
-    // The only method to be called from outside the object.
     this.init = function () {
-        // Counters
-        var i, p, k = 0;
-        // DOM-nodes
+
+        // Counters and that
+        var i, p,
+            k = 0;
+
         var img, anchor, td, tr,
             table = document.createElement("table"),
             div = document.querySelector("div#" + memoryID);
@@ -21,22 +22,35 @@ function MemoryBoard(memoryID, rows, cols) {
         for (i = 1; i <= rows; i += 1) {
             tr = document.createElement("tr");
             for (p = 1; p <= cols; p += 1) {
+
                 img = document.createElement("img");
-                img.setAttribute("src", "pics/" + this.memoryCards[k] + ".png");
+                img.setAttribute("src", "pics/0.png");
                 anchor = document.createElement("a");
+                anchor.setAttribute("class", this.memoryCards[k])
+                anchor.setAttribute("href", "#");
                 td = document.createElement("td");
                 anchor.appendChild(img);
                 td.appendChild(anchor);
                 tr.appendChild(td);
+                
+                anchor.addEventListener("click", function (e) {
+                    e = e || window.event; // IE-fix
+                    e.preventDefault();
+                    e.currentTarget.firstChild.setAttribute("src", "pics/" + e.currentTarget.getAttribute("class") + ".png");
+                }, false);
+
                 // The picture counter
                 k += 1;
             }
             table.appendChild(tr);
-        }
+        } // for-loop
+
         div.appendChild(table);
+
+
     };
 }
 
 window.onload = function () {
-    new MemoryBoard("memoryBoard1", 1, 6).init();
+    new MemoryBoard("memoryBoard1", 3, 4).init();
 };
